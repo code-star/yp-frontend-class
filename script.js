ff730c85b34c0a36694d997d4eb86a7e0aec3cc1
Maik Diepenbroek <maik.diepenbroek@rabobank.nl>
2016-04-27 18:56:45 +0200

    Added some javascript to week_1, template loading and basic responsive stuff

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function loadAboutPageContent() {
-  document.getElementById("mainContent").innerHTML = "<h2>Welcome to the about page</h2><p>Learn more about us!</p>";
+var xhr = new XMLHttpRequest();
+
+function loadPageContent(page) {
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            document.getElementById("mainContent").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'templates/'+page+'.html');
+    xhr.send();
+}
+
+function displayIncome() {
+    var incomeDisplay = document.querySelector('#incomeDisplay');
+    var incomeRange = document.querySelector('#incomeRange');
+
+    incomeDisplay.innerHTML = incomeRange.value;
+}
+
+function displayPartnerIncome() {
+    var incomePartnerDisplay = document.querySelector('#incomePartnerDisplay');
+    var incomePartnerRange = document.querySelector('#incomePartnerRange');
+
+    incomePartnerDisplay.innerHTML = incomePartnerRange.value;
+    changeIncomeResult(incomePartnerRange.value);
+}
+
+function changeIncomeResult(partnerIncome) {
+    var incomeResultAmountElement = document.querySelector('#incomeResultAmount');
+    var incomeAmount = incomeResultAmountElement.textContent;
+
+    if(parseInt(partnerIncome) < parseInt(incomeAmount)) {
+        incomeResultAmountElement.innerHTML = parseInt(incomeAmount) - Math.round(Math.random() * 10000);
+    }
+    else {
+        incomeResultAmountElement.innerHTML = parseInt(incomeAmount) + Math.round(Math.random() * 10000);
+    }
+
 }
